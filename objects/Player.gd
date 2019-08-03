3d17165673ef0fa626df0de673d9f6a7df2712eb
clowd <>
2019-08-02 20:09:04 -0400

    Initial commit

---
+++ objects/Player.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+export var speed = 400  # How fast the player will move (pixels/sec).
+export var score = 1000
+var screen_size  # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    var velocity = Vector2()  # The player's movement vector.
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+        
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+        
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+        
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+        
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+
+        
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screen_size.x)
+    position.y = clamp(position.y, 0, screen_size.y)
